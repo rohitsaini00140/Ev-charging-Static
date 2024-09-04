1b29c7358bc9c558339b08e5c38daed88ee95aa5
Rohit Saini <rohit@hhpsoftware.in>
2024-09-04 11:52:33 +0530

    setup the project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './Pages/Header';
 import Footer from './Pages/Footer';
 import Home from './Pages/Home';
 
 function App() {
   return (
     <Router>
-    <Header />
     <Routes>
      <Route path="/" element={<Home />} />
     </Routes>
