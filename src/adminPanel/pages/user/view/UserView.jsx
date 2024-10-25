5f2a5401456f506a7a57071b027e660a86b87574
Rohit Saini <rohit@hhpsoftware.in>
2024-10-25 12:33:27 +0530

    working on designing

--- src/adminPanel/pages/user/view/UserView.jsx
+++ src/adminPanel/pages/user/view/UserView.jsx
@@ -1,45 +1,37 @@
-import Card from '@mui/material/Card';
-import Stack from '@mui/material/Stack';
-import Table from '@mui/material/Table';
-import Button from '@mui/material/Button';
-import Container from '@mui/material/Container';
-import TableBody from '@mui/material/TableBody';
-import Typography from '@mui/material/Typography';
-import TableContainer from '@mui/material/TableContainer';
-import Iconify from '../../../component/Iconify';
-import Scrollbar from '../../../component/scrollbar/Scrollbar';
-import UserTableHead from './UserTableHead';
-import UserTableToolbar from './UserTableToolbar';
-import { Link } from 'react-router-dom';
-import UserTableRow from './UserTableRow';
-import TablePagination from '../../../component/TablePagination';
-import { useGetUsersQuery } from '../../../../globalState/user/userApis';
-import { useDispatch, useSelector } from 'react-redux';
-import { setUserListPageNo } from '../../../../globalState/user/userSlice';
-import { StyledTableCell, StyledTableRow } from '../../../component/tableStyle';
-import { useLocation, useNavigate } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import Alertbar from '../../../component/Alertbar';
+import Card from "@mui/material/Card";
+import Stack from "@mui/material/Stack";
+import Table from "@mui/material/Table";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import TableBody from "@mui/material/TableBody";
+import Typography from "@mui/material/Typography";
+import TableContainer from "@mui/material/TableContainer";
+import Iconify from "../../../component/Iconify";
+import Scrollbar from "../../../component/scrollbar/Scrollbar";
+import UserTableHead from "./UserTableHead";
+import UserTableToolbar from "./UserTableToolbar";
+import { Link } from "react-router-dom";
+import UserTableRow from "./UserTableRow";
+import TablePagination from "../../../component/TablePagination";
+import { useGetUsersQuery } from "../../../../globalState/user/userApis";
+import { useDispatch, useSelector } from "react-redux";
+import { setUserListPageNo } from "../../../../globalState/user/userSlice";
+import { StyledTableCell, StyledTableRow } from "../../../component/tableStyle";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import Alertbar from "../../../component/Alertbar";
 
 // ----------------------------------------------------------------------
 
-
-const role = JSON.parse(sessionStorage.getItem("role"))
-
-
+const role = JSON.parse(sessionStorage.getItem("role"));
 
 function UserView() {
-
-
-
-  const { logInRole } = useSelector(state => state.role)
-
-
+  const { logInRole } = useSelector((state) => state.role);
 
   const [snackbar, setSnackbar] = useState({
     open: false,
-    message: '',
-    severity: 'success'
+    message: "",
+    severity: "success",
   });
 
   const location = useLocation();
@@ -50,47 +42,60 @@ function UserView() {
       setSnackbar({
         open: true,
         message: location.state.message,
-        severity: location.state.severity
+        severity: location.state.severity,
       });
       navigate(location.pathname, { replace: true, state: {} });
     }
   }, [location.state, navigate]);
 
+  const dispatch = useDispatch();
 
-  const dispatch = useDispatch()
+  const { pageNo, userName, status } = useSelector((state) => state.user);
 
-  const { pageNo, userName, status } = useSelector(state => state.user)
+  const { role } = useSelector((state) => state.role);
 
-  const { role } = useSelector(state => state.role)
+  const { clusterName } = useSelector((state) => state.cluster);
 
-  const { clusterName } = useSelector(state => state.cluster)
+  const {
+    data: allUsers,
+    isSuccess: userSuccess,
+    isLoading,
+  } = useGetUsersQuery({
+    page: pageNo,
+    name: userName,
+    status,
+    clusterName,
+    role_id: role,
+  });
 
-  const { data: allUsers, isSuccess: userSuccess, isLoading } = useGetUsersQuery({ page: pageNo, name: userName, status, clusterName, role_id: role });
+  const allUserData =
+    logInRole?.user?.role?.name === "Superadmin"
+      ? userSuccess && allUsers?.data
+      : [];
 
-  // const allUserData = userSuccess && (allUsers?.data).map(ele => ele?.name === role?.user?.name);
-
-  const allUserData = logInRole?.user?.role?.name === "Superadmin" ? (userSuccess && (allUsers?.data)) : (userSuccess && allUsers?.data?.filter(ele => ele?.name === logInRole?.user?.name))
-
+  // const allUserData =
+  //   logInRole?.user?.role?.name === "Superadmin"
+  //     ? userSuccess && allUsers?.data
+  //     : userSuccess &&
+  //       allUsers?.data?.filter((ele) => ele?.name === logInRole?.user?.name);
 
   const paginationData = userSuccess && allUsers;
 
-  const { last_page } = paginationData
+  const { last_page } = paginationData;
 
   const handlePageChange = (event, value) => {
     dispatch(setUserListPageNo(value));
   };
 
-
   const handleCloseSnackbar = (event, reason) => {
-    if (reason === 'clickaway') {
+    if (reason === "clickaway") {
       return;
     }
     setSnackbar((prevState) => ({
       ...prevState,
-      open: false
+      open: false,
     }));
   };
-
 
   return (
     <>
@@ -101,56 +106,75 @@ function UserView() {
           justifyContent="space-between"
           m={3}
         >
-          <Typography variant="h4" color="white">Users</Typography>
+          <Typography variant="h4" color="white">
+            Users
+          </Typography>
           <Link to={"/admin/user/add"}>
             <Button
               variant="contained"
               sx={{
                 bgcolor: "#20c997",
-                boxShadow: 'none',
-                padding: '10px 10px',
-                "&:hover": { bgcolor: "#20c997" }
+                boxShadow: "none",
+                padding: "10px 10px",
+                "&:hover": { bgcolor: "#20c997" },
               }}
-              startIcon={<Iconify icon="eva:plus-fill" />}>
+              startIcon={<Iconify icon="eva:plus-fill" />}
+            >
               New User
             </Button>
           </Link>
         </Stack>
-        <Card sx={{ bgcolor: "rgb(29, 40, 44)", boxShadow: '0px 4px 12px rgba(87, 179, 62, 0.2)' }}>
+        <Card
+          sx={{
+            bgcolor: "rgb(29, 40, 44)",
+            boxShadow: "0px 4px 12px rgba(87, 179, 62, 0.2)",
+          }}
+        >
           <UserTableToolbar />
           {isLoading ? (
-            <Stack alignItems="center" justifyContent="center" sx={{ minHeight: 300, padding: 4 }}>
-              <Typography color="white" sx={{ mt: 2 }}>Loading...</Typography>
+            <Stack
+              alignItems="center"
+              justifyContent="center"
+              sx={{ minHeight: 300, padding: 4 }}
+            >
+              <Typography color="white" sx={{ mt: 2 }}>
+                Loading...
+              </Typography>
             </Stack>
           ) : (
             <Scrollbar>
-              <TableContainer sx={{ overflow: 'unset' }}>
+              <TableContainer sx={{ overflow: "unset" }}>
                 <Table sx={{ minWidth: 800 }}>
                   <UserTableHead allUserData={allUserData} />
                   <TableBody>
-                    {allUserData.length > 0 ?
+                    {allUserData.length > 0 ? (
                       <UserTableRow
                         currentpage={pageNo}
                         allUserData={allUserData}
                       />
-                      :
-                      (
-                        <StyledTableRow>
-                          <StyledTableCell colSpan={10} align="center" sx={{ border: "1px solid red", padding: "2rem" }}>
-                            <Typography color="white">No Data Found</Typography>
-                          </StyledTableCell>
-                        </StyledTableRow>
-                      )}
+                    ) : (
+                      <StyledTableRow>
+                        <StyledTableCell
+                          colSpan={10}
+                          align="center"
+                          sx={{ border: "1px solid red", padding: "2rem" }}
+                        >
+                          <Typography color="white">No Data Found</Typography>
+                        </StyledTableCell>
+                      </StyledTableRow>
+                    )}
                   </TableBody>
                 </Table>
               </TableContainer>
             </Scrollbar>
           )}
-          {(allUserData.length > 0) && <TablePagination
-            count={last_page}
-            onPageChange={handlePageChange}
-            page={pageNo}
-          />}
+          {allUserData.length > 0 && (
+            <TablePagination
+              count={last_page}
+              onPageChange={handlePageChange}
+              page={pageNo}
+            />
+          )}
         </Card>
       </Container>
       <Alertbar
@@ -158,7 +182,7 @@ function UserView() {
         onClose={handleCloseSnackbar}
         severity={snackbar.severity}
         message={snackbar.message}
-        position={{ vertical: 'top', horizontal: 'center' }}
+        position={{ vertical: "top", horizontal: "center" }}
         sx={{ mt: "5rem" }}
       />
     </>
